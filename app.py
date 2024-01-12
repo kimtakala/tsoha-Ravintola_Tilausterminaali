1632cb0d746def3d6b77fa11500fad1f357032ae
Kim Takala <kim.takala@helsinki.fi>
2024-01-12 16:04:41 +0200

    clearing the database to make way for the actual assignment. this also includes the changes in 'app.py' that are only relating to previously done example exercises.

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from os import getenv
-from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+from flask import Flask, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT content FROM messages"))
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages) 
+    return render_template("index.html")
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
 
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(text(sql), {"content":content})
-    db.session.commit()
+@app.route("/logout")
+def logout():
+    del session["username"]
     return redirect("/")
