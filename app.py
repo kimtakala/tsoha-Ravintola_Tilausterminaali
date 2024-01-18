a1cb1b4f4c46d404caabfcb0076a81bc1824d8c4
Kim Takala <kim.takala@helsinki.fi>
2024-01-18 13:34:39 +0200

    updated login function, checking for username and password

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from os import getenv
 from flask import Flask, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.sql import text #! MUISTA KÄYTTÄÄ executen sisällä: execute(text('sql'))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -12,8 +16,20 @@ def index():
 @app.route("/login",methods=["POST"])
 def login():
     username = request.form["username"]
-    password = request.form["password"]
-    # TODO: check username and password
+    passwordtry = generate_password_hash(request.form["password"])
+    sql = "SELECT id, passwordhash, admin FROM users WHERE username=:username AND removed != TRUE"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()
+    if not user:
+        session['incorrect_values'] = True
+    else:
+        hash_value = user.passwordhash
+        if check_password_hash(hash_value, passwordtry):
+            session['logged in'] = True
+            session['admin'] = user.admin
+        else:
+            session['incorrect values'] = True
+
     session["username"] = username
     return redirect("/")
 
