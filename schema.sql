bec8211b7a39cce7ac5941d761bbf51b15f0c21a
Kim Takala <kim.takala@helsinki.fi>
2024-03-19 22:03:22 +0200

    added cleanup functionality to start

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 12.15
 -- Dumped by pg_dump version 12.15
 
+DROP TABLE food, categories, foodinfo, orders, users, food_in_category, food_in_order;
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
